6720107d5f5374644ded83feeb2f6b24261bc475
Huandaotien <foxmunder1234@gmail.com>
2024-03-05 10:18:21 +0700

    create

---
+++ include/custom_planner/custom_planner.h
@@ -0,0 +1,162 @@
+#ifndef CUSTOM_PLANNER_H
+#define CUSTOM_PLANNER_H
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+// ROS
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <visualization_msgs/Marker.h>
+
+// Costmap used for the map representation
+#include <costmap_2d/costmap_2d_ros.h>
+
+// sbpl headers
+#include <sbpl/headers.h>
+
+// global representation
+#include <nav_core/base_global_planner.h>
+
+// tf
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2/convert.h>
+#include <tf2_ros/buffer.h>
+
+// internal lib
+#include <custom_planner/pose.h>
+#include <custom_planner/pathway.h>
+
+#include <thread>
+#include <boost/thread.hpp>
+
+namespace custom_planner{
+
+struct userParams{
+    userParams(){
+        directory_to_save_paths = "/init/paths";             
+        pathway_filename = "pathway.txt";
+        current_pose_topic_name = "/amcl_pose";
+        map_frame_id = "map";
+    }
+    string directory_to_save_paths;
+    string pathway_filename;
+    string current_pose_topic_name;
+    string map_frame_id;
+};
+
+class CustomPlanner : public nav_core::BaseGlobalPlanner{
+public:
+  
+  /**
+   * @brief  Default constructor for the NavFnROS object
+   */
+  CustomPlanner();
+
+  
+  /**
+   * @brief  Constructor for the CustomPlanner object
+   * @param  name The name of this planner
+   * @param  costmap_ros A pointer to the ROS wrapper of the costmap to use
+   */
+  CustomPlanner(std::string name, costmap_2d::Costmap2DROS* costmap_ros);
+
+
+  /**
+   * @brief  Initialization function for the CustomPlanner object
+   * @param  name The name of this planner
+   * @param  costmap_ros A pointer to the ROS wrapper of the costmap to use
+   */
+  virtual void initialize(std::string name, 
+                          costmap_2d::Costmap2DROS* costmap_ros);
+  
+  /**
+   * @brief Given a goal pose in the world, compute a plan
+   * @param start The start pose 
+   * @param goal The goal pose 
+   * @param plan The plan... filled by the planner
+   * @return True if a valid plan was found, false otherwise
+   */
+  virtual bool makePlan(const geometry_msgs::PoseStamped& start, 
+                        const geometry_msgs::PoseStamped& goal, 
+                        std::vector<geometry_msgs::PoseStamped>& plan);
+
+  virtual ~CustomPlanner(){};
+
+private:
+  unsigned char costMapCostToSBPLCost(unsigned char newcost);
+  void publishStats(int solution_cost, int solution_size, 
+                    const geometry_msgs::PoseStamped& start, 
+                    const geometry_msgs::PoseStamped& goal);
+
+  unsigned char computeCircumscribedCost();
+
+  static void transformFootprintToEdges(const geometry_msgs::Pose& robot_pose,
+                                        const std::vector<geometry_msgs::Point>& footprint,
+                                        std::vector<geometry_msgs::Point>& out_footprint);
+
+  void getFootprintList(const std::vector<EnvNAVXYTHETALAT3Dpt_t>& sbpl_path, const std::string& path_frame_id,
+                        visualization_msgs::Marker& ma);
+
+ 
+
+  inline double getYaw(double x, double y, double z, double w);
+
+  bool loadPathwayData(const string& filename);
+
+  bool findNearestPoseOfPath(vector<Pose>& posesOnPathWay, Pose& PoseToCheck, Pose& PoseResult);
+  
+  Pathway* pathway;   
+  userParams* userParams_; 
+  Pose* startPose;
+  vector<Pose> posesOnPathWay;      
+  Pose start_on_path;      
+  uint16_t start_on_path_index;
+  bool initialized_;
+
+  SBPLPlanner* planner_;
+  EnvironmentNAVXYTHETALAT* env_;
+  
+  std::string planner_type_; /**< sbpl method to use for planning.  choices are ARAPlanner and ADPlanner */
+
+  double allocated_time_; /**< amount of time allowed for search */
+  double initial_epsilon_; /**< initial epsilon for beginning the anytime search */
+
+  std::string environment_type_; /** what type of environment in which to plan.  choices are 2D and XYThetaLattice. */ 
+  std::string cost_map_topic_; /** what topic is being used for the costmap topic */
+
+  bool forward_search_; /** whether to use forward or backward search */
+  std::string primitive_filename_; /** where to find the motion primitives for the current robot */
+  int force_scratch_limit_; /** the number of cells that have to be changed in the costmap to force the planner to plan from scratch even if its an incremental planner */
+
+  unsigned char lethal_obstacle_;
+  unsigned char inscribed_inflated_obstacle_;
+  unsigned char circumscribed_cost_;
+  unsigned char sbpl_cost_multiplier_;
+
+  bool publish_footprint_path_;
+  int visualizer_skip_poses_;
+
+  bool allow_unknown_;
+
+  std::string name_;
+  costmap_2d::Costmap2DROS* costmap_ros_; /**< manages the cost map for us */
+  std::vector<geometry_msgs::Point> footprint_;
+  unsigned int current_env_width_;
+  unsigned int current_env_height_;
+
+  ros::Publisher plan_pub_;
+  ros::Publisher stats_publisher_;
+  
+  ros::Publisher sbpl_plan_footprint_pub_;  
+  boost::mutex mutex_;
+};
+};
+
+#endif
+
---
+++ src/pose.cc
@@ -0,0 +1,42 @@
+#include <custom_planner/pose.h>
+
+namespace custom_planner
+{
+
+bool Pose::SavePoseAsFile(const string& file_name)
+{
+    bool result = false;
+    ofstream file(file_name);
+    if (file.is_open())
+    {
+        file << x_ << " " << y_ << " "<< yaw_ << "\n";
+        file.close();
+        result = true;
+    }
+    else
+    {
+        result = false;
+    }
+    return result;
+}
+
+bool Pose::LoadPoseFromFile(const string& file_name)
+{
+    bool result = false;
+    ifstream file(file_name);
+    if (file.is_open())
+    {
+        double x, y, yaw;
+        file >> x >> y >> yaw;
+        setPose(x, y, yaw);
+        file.close();
+        result = true;
+    }
+    else
+    {
+       result = false;
+    }
+    return result;
+}
+
+}
